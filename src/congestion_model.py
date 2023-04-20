857f44c1e8c640b3b53ff816dec45e88dfc35cbc
Zixuan Yan <ZYan@win.cccis.com>
2023-04-20 16:39:41 -0400

    Initialize the spark app

---
+++ src/congestion_model.py
@@ -0,0 +1,31 @@
+def simple_congestion_model(expected_speed: int, actual_speed: int) -> int:
+    """
+    Evalute congestion level of a road segment depending on the difference between expected and actual speed.
+    This model is a naive version, which only considers the speed difference.
+    Reference: https://developer.tomtom.com/traffic-api/documentation/traffic-flow/raster-flow-tiles
+
+    Params:
+        expected_speed: Expected speed of a road segment
+        actual_speed: Actual speed of a road segment
+
+    Returns:
+        congestion_level: Congestion level of a road segment
+    """
+
+    speed_percentage = actual_speed / expected_speed
+
+    assert (
+        speed_percentage >= 0 and speed_percentage <= 1
+    ), f"Speed percentage must be between 0 and 1, got {speed_percentage}"
+
+    # 0 is reserved for road closed
+    if speed_percentage < 0.15:
+        congestion_level = 1
+    elif speed_percentage < 0.35:
+        congestion_level = 2
+    elif speed_percentage < 0.75:
+        congestion_level = 3
+    else:
+        congestion_level = 4
+
+    return congestion_level
