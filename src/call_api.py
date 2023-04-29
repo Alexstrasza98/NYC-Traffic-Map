82f4e6fdcc1629346ddec0c2d3344a7f4fb17641
Zixuan Yan <ZYan@win.cccis.com>
2023-04-29 14:06:42 -0400

    Add spark processing for traffic

--- src/call_api.py
+++ src/call_api.py
@@ -1,19 +1,19 @@
-from src.tomtom import get_traffic_data, get_incident_data
-from src.utils import write_json
+from tomtom import get_traffic_data, get_incident_data
+from utils import write_json
 
 if __name__ == "__main__":
-    # print("Requesting traffic data...")
-    # with open("./data/coordinates_manhattan.txt", "r") as f:
-    #     coordinates = f.readlines()
+    print("Requesting traffic data...")
+    with open("./data/coordinates_manhattan.txt", "r") as f:
+        coordinates = f.readlines()
 
-    # coordinates = [coordinate.strip() for coordinate in coordinates]
-    # zoom = 15
+    coordinates = [coordinate.strip() for coordinate in coordinates]
+    zoom = 15
 
-    # # Only fetch first 1000 samples
-    # traffic_data = get_traffic_data(coordinates[:1000], zoom)
-    # write_json(traffic_data, "./data/traffic.json")
+    # Only fetch first 1000 samples
+    traffic_data = get_traffic_data(coordinates[:1000], zoom)
+    write_json(traffic_data, "./data/traffic_tomtom.json")
 
     print("Requesting incident data...")
     bbox = "-74.010626,40.744806,-73.960532,40.756587"
     incident_data = get_incident_data(bbox)
-    write_json(incident_data, "./data/incident.json")
+    write_json(incident_data, "./data/incident_tomtom.json")
