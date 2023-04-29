3fa9da59993281f30a344db0aa3307ad335e0307
Zixuan Yan <ZYan@win.cccis.com>
2023-04-29 17:57:23 -0400

    Extend incidents to all Manhattan

--- src/call_api.py
+++ src/call_api.py
@@ -2,18 +2,24 @@ from tomtom import get_incident_data, get_traffic_data
 from utils import write_json
 
 if __name__ == "__main__":
-    print("Requesting traffic data...")
-    with open("./data/coordinates_manhattan.txt", "r") as f:
-        coordinates = f.readlines()
+    # print("Requesting traffic data...")
+    # with open("./data/coordinates_manhattan.txt", "r") as f:
+    #     coordinates = f.readlines()
 
-    coordinates = [coordinate.strip() for coordinate in coordinates]
-    zoom = 15
+    # coordinates = [coordinate.strip() for coordinate in coordinates]
+    # zoom = 15
 
-    # Only fetch first 1000 samples
-    traffic_data = get_traffic_data(coordinates[:1000], zoom)
-    write_json(traffic_data, "./data/traffic_tomtom.json")
+    # # Only fetch first 1000 samples
+    # traffic_data = get_traffic_data(coordinates[:1000], zoom)
+    # write_json(traffic_data, "./data/traffic_tomtom.json")
 
     print("Requesting incident data...")
-    bbox = "-74.010626,40.744806,-73.960532,40.756587"
-    incident_data = get_incident_data(bbox)
+    incident_data = []
+    bboxs = [
+        "-74.017191,40.701654,-73.970910,40.750482",
+        "-74.003519,40.756170,-73.944322,40.784978",
+        "40.789491 -73.980009,-73.930612,40.814190",
+    ]
+    for bbox in bboxs:
+        incident_data.append(get_incident_data(bbox))
     write_json(incident_data, "./data/incident_tomtom.json")
