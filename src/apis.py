6bcd2c320cc1cc6708f14097a9784e097e60ac09
Zixuan Yan <zixuanyan@Sophies-MacBook-Air-89.local>
2023-05-01 23:48:24 -0400

    Change to focus on columbia area

--- src/apis.py
+++ src/apis.py
@@ -15,9 +15,9 @@ API_KEY = os.getenv("TOMTOM_API_KEY")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 TRAFFIC_URL = "https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json?point={}&zoom={}&key={}"
 INCIDENT_URL = "https://api.tomtom.com/traffic/services/5/incidentDetails?key={}&bbox={}&language=en-GB&t=1111&timeValidityFilter=present"
-WEARTHER_URL = "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={}&lon={}&appid={}"
-
-
+WEARTHER_URL = (
+    "https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={}&lon={}&appid={}"
+)
 
 
 def get_traffic_data(coordinates: List[Tuple], zoom: int) -> List[Dict]:
@@ -76,7 +76,6 @@ def get_incident_data(bbox: str) -> List[Dict]:
 
     # Send request and parse response
     url = INCIDENT_URL.format(API_KEY, bbox)
-    print(url)
     response = requests.get(url)
     data = json.loads(response.text)["incidents"]
 
@@ -97,58 +96,59 @@ async def get_traffic_data_async(coord_rdd, zoom):
     async with aiohttp.ClientSession() as session:
         responses = []
         for line in coord_rdd.map(lambda row: row[0]).collect():
-            single_response = asyncio.ensure_future(single_call(session, line, zoom, API_KEY))
+            single_response = asyncio.ensure_future(
+                single_call(session, line, zoom, API_KEY)
+            )
             responses.append(single_response)
         tomtom = await asyncio.gather(*responses)
-    # print(tomtom)
     return tomtom
+
 
 async def single_call(session, coord, zoom, API_KEY):
     url_traffic = TRAFFIC_URL.format(coord, zoom, API_KEY)
-    # print(url_traffic)
     async with session.get(url_traffic) as response:
-        content_type = response.headers.get('content-type')
+        content_type = response.headers.get("content-type")
         if content_type != "text/xml":
             result_data = await response.json()
         else:
             # if detects xml:
             #   scenario 1: ran out of all the requests, then probably should just finish everything. (here will it be reseted for another 15 minus?)
             #   scenatio 2: ran out of the request for 1 second, wait 1 second.
-            #seem like the best solution is to downsample (condsidering the demo and limited requests)
+            # seem like the best solution is to downsample (condsidering the demo and limited requests)
             time.sleep(1)
-        print(content_type)
-        print(type(content_type))
         try:
             return result_data
         except:
             return None
 
+
 async def get_weather_data_async(coord_rdd):
     async with aiohttp.ClientSession() as session:
         responses = []
         for coordinate in coord_rdd.map(lambda row: row[0]).collect():
-            single_response = asyncio.ensure_future(single_call_weather(session, coordinate))
+            single_response = asyncio.ensure_future(
+                single_call_weather(session, coordinate)
+            )
             responses.append(single_response)
         weather = await asyncio.gather(*responses)
-    print(weather)
+
     return weather
+
 
 async def single_call_weather(session, coordinate: List[Tuple]) -> List[Dict]:
     lat, lon = coordinate.split(",")
     url_weather = WEARTHER_URL.format(lat, lon, WEATHER_API_KEY)
-    print(url_weather)
     async with session.get(url_weather) as response:
-        content_type = response.headers.get('content-type')
+        content_type = response.headers.get("content-type")
         if content_type != "text/xml":
             result_data = await response.json()
-            print(result_data)
-            current_data = result_data['list'][0]
-            rain = current_data.get('rain', {}).get('1h', 0)
-            wind_speed = current_data['wind']['speed']
-            temp = current_data['main']['temp']
-            humidity = current_data['main']['humidity']
-            visibility = current_data.get('visibility', 0)
-            weather = current_data['weather'][0]['main']
+            current_data = result_data["list"][0]
+            rain = current_data.get("rain", {}).get("1h", 0)
+            wind_speed = current_data["wind"]["speed"]
+            temp = current_data["main"]["temp"]
+            humidity = current_data["main"]["humidity"]
+            visibility = current_data.get("visibility", 0)
+            weather = current_data["weather"][0]["main"]
 
             weather_data = {
                 "coordinate": coordinate,
@@ -161,8 +161,7 @@ async def single_call_weather(session, coordinate: List[Tuple]) -> List[Dict]:
             }
         else:
             time.sleep(1)
-        print(content_type)
-        print(type(content_type))
+
         try:
             return weather_data
         except:
--- src/call_api.py
+++ src/call_api.py
@@ -8,12 +8,10 @@ from apis import (
     get_weather_data_async,
 )
 
-import aiohttp
 from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 
-from tomtom import get_incident_data, get_traffic_data, get_traffic_data_async
 from utils import write_json
 
 load_dotenv()
@@ -22,7 +20,7 @@ API_KEY = os.getenv("TOMTOM_API_KEY")
 
 
 def get_data_async(sc):
-    text_file = sc.read.text("data/coordinates_manhattan_test.txt")
+    text_file = sc.read.text("data/coordinates_columbia.txt")
     print("Requesting traffic data...")
 
     loop = asyncio.get_event_loop()
@@ -35,14 +33,14 @@ def get_data_async(sc):
 
 def get_incident_middlefile():
     print("Requesting incident data...")
-    bbox = "-74.010626,40.744806,-73.960532,40.756587"
+    bbox = "-73.981515,40.792483,-73.956882,40.806128"
     incident_data = get_incident_data(bbox)
     return incident_data
 
 
 def get_weather_async(sc):
     print("Requesting weather data...")
-    text_weather_file = sc.read.text("../data/coord_manhatan_test.txt")
+    text_weather_file = sc.read.text("data/coordinates_columbia.txt")
     weather_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(weather_loop)
     res_weather = weather_loop.run_until_complete(
@@ -50,41 +48,3 @@ def get_weather_async(sc):
     )
     weather_loop.close()
     return res_weather
-
-
-if __name__ == "__main__":
-    # get the request and data loading part into pyspark session
-    spark_txt = SparkSession.builder.appName("ReadCoordFile").getOrCreate()
-    text_file = spark_txt.read.text("data/coordinates_manhattan_test.txt")
-    print("Requesting traffic data...")
-
-    # loop = asyncio.get_event_loop()
-    # res = loop.run_until_complete(get_traffic_data_async(text_file.rdd, "15"))
-    # loop.close()
-
-    # print("Requesting weather data...")
-    res = get_weather_async()
-    # weather_loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(weather_loop)
-    # res_weather = weather_loop.run_until_complete(get_weather_data_async(text_file.rdd))
-    # weather_loop.close()
-    print(res)
-    # getting incident data
-    get_incident_middlefile()
-
-    # Last version with middle files
-    # with open("./data/coordinates_manhattan.txt", "r") as f:
-    #     coordinates = f.readlines()
-
-    #
-    # coordinates = [coordinate.strip() for coordinate in coordinates]
-    # zoom = 15
-    #
-    # # Only fetch first 1000 samples
-    # traffic_data = get_traffic_data(coordinates[:1000], zoom)
-    # write_json(traffic_data, "./data/traffic_tomtom.json")
-    #
-    # print("Requesting incident data...")
-    # bbox = "-74.010626,40.744806,-73.960532,40.756587"
-    # incident_data = get_incident_data(bbox)
-    # write_json(incident_data, "./data/incident_tomtom.json")
